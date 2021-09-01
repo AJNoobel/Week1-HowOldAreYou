a539b2ef740095306f1feea463f4906f1d99cee3
Opilane TTHK <opilane@tthk.ee>
2021-09-01 12:14:16 +0300

    read inputs

---
+++ FullName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis su nimi uh?");
+            string Userfirstname = Console.ReadLine();
+            Console.WriteLine("Mis teine nimi uhhh?");
+            string UserLastName = Console.ReadLine();
+            Console.WriteLine($"tere {Userfirstname} {UserLastName}!");
+            Console.ReadLine();   
+
+
+
+        }
+    }
+}
---
+++ HowOldAreYou/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HowOldAreYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("What is your name?");
+            //ReadLine() to read the user input from the console
+            string UserName = Console.ReadLine();
+            //Console.WriteLine("Hello, " + UserName + "!");
+            //string interpolation
+            Console.WriteLine($"Hello,{UserName}");
+
+            Console.WriteLine("How Old Are You? Enter Digits Only:");
+
+            int Age = Int32.Parse(Console.ReadLine());
+            int YearOfBirth = 2021 - Age;
+
+            Console.WriteLine($"You Were Born In {YearOfBirth}");
+
+
+            Console.Read();
+        }
+    }
+}
